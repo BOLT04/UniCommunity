e2ad77e755de4594ba79d14c386fcbe2579a8b64
David <didicp4@gmail.com>
2019-03-18 15:03:06 +0000

    Initial NavBar

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-import './App.css';
+import NavBar from './NavBar'
+//import Login from './Login'
+//import CreateBoard from './CreateBoard'
+//import Board from './Board'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
-      </div>
-    );
+      <Router>   
+        <div className="App">
+          <div className="ui container">
+            <NavBar /> 
+          </div>
+        </div> 
+      </Router>
+    )
   }
 }
 
-export default App;
+export default App
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import { Link } from "react-router-dom"
+
+class NavBar extends Component {
+  render() {
+    return (
+      <div className="ui secondary menu">
+        <a className="active item">
+          Home
+        </a>
+        <a className="item">
+          Messages
+        </a>
+        <Link to="/board/create" className="item">
+            Create Board
+        </Link>
+        <div className="right menu">
+          <div className="item">
+            <div className="ui icon input">
+              <input type="text" placeholder="Search..." />
+              <i className="search link icon"></i>
+            </div>
+          </div>
+          
+          <Link to="/login">
+            <button className="ui primary basic button">Log in</button>
+          </Link>
+          <button className="ui secondary basic button">Sign up</button>  
+
+          <button className="ui red basic button">Logout</button>        
+
+        </div>
+      </div>
+    )
+  }
+}
+
+export default NavBar
