9ffea1b7e115152548a889ec34d3328f45a72429
Sergiu Cozma <sergiucozma94@hotmail.com>
2019-04-14 12:57:18 +0100

    Incorporated JPA and H2 in-memory database

--- server/src/main/kotlin/pt/isel/g20/unicommunity/UniCommunityApplication.kt
+++ server/src/main/kotlin/pt/isel/g20/unicommunity/UniCommunityApplication.kt
@@ -1,8 +1,12 @@
 package pt.isel.g20.unicommunity
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
+@EnableJpaRepositories("pt.isel.g20.unicommunity.repository")
+@EntityScan("pt.isel.g20.unicommunity.user.model")
 @SpringBootApplication
 class UniCommunityApplication
 
