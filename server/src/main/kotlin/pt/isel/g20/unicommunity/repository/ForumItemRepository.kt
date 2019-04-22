45317efc66c07be0ab086d6f1fe6676f209ee5a8
Sergiu Cozma <sergiucozma94@hotmail.com>
2019-04-22 13:42:43 +0100

    Added @Transactional annotations to repos to guarantee atomic actions

--- server/src/main/kotlin/pt/isel/g20/unicommunity/repository/ForumItemRepository.kt
+++ server/src/main/kotlin/pt/isel/g20/unicommunity/repository/ForumItemRepository.kt
@@ -1,6 +1,10 @@
 package pt.isel.g20.unicommunity.repository
 
 import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
 import pt.isel.g20.unicommunity.forumItem.model.ForumItem
+import javax.transaction.Transactional
 
+@Repository
+@Transactional
 interface ForumItemRepository : CrudRepository<ForumItem, Long>
--- server/src/main/kotlin/pt/isel/g20/unicommunity/repository/TemplateRepository.kt
+++ server/src/main/kotlin/pt/isel/g20/unicommunity/repository/TemplateRepository.kt
@@ -1,6 +1,10 @@
 package pt.isel.g20.unicommunity.repository
 
 import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
 import pt.isel.g20.unicommunity.template.model.Template
+import javax.transaction.Transactional
 
+@Repository
+@Transactional
 interface TemplateRepository : CrudRepository<Template, Long>
