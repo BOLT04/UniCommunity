3b521fd575fa513662a3aef2593ee94ce72beda4
Sergiu Cozma <sergiucozma94@hotmail.com>
2019-05-07 22:05:22 +0100

    Added CORS Filter

--- server/src/main/kotlin/pt/isel/g20/unicommunity/board/BoardController.kt
+++ server/src/main/kotlin/pt/isel/g20/unicommunity/board/BoardController.kt
@@ -19,7 +19,6 @@ import org.springframework.security.core.context.SecurityContextHolder
 import pt.isel.g20.unicommunity.hateoas.Uri.BOARD_MEMBERS
 import pt.isel.g20.unicommunity.user.model.User
 
-@CrossOrigin
 @PreAuthorize("hasRole('TEACHER')")
 @RestController
 @RequestMapping(produces = ["application/hal+json", "application/json", "application/vnd.collection+json"])
