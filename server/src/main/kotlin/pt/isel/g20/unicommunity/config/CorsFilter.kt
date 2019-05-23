392e5a348298de06d44432e93180b1fadde40382
Sergiu Cozma <sergiucozma94@hotmail.com>
2019-05-23 15:08:06 +0100

    Added a new CorsFilter. Tested and working

--- server/src/main/kotlin/pt/isel/g20/unicommunity/config/CorsFilter.kt
+++ server/src/main/kotlin/pt/isel/g20/unicommunity/config/CorsFilter.kt
@@ -1,34 +1,28 @@
 package pt.isel.g20.unicommunity.config
 
-import org.springframework.context.annotation.Configuration
-import javax.servlet.Filter
+import org.springframework.stereotype.Component
 import javax.servlet.http.HttpServletResponse
-import javax.servlet.ServletException
-import javax.servlet.FilterConfig
 import java.io.IOException
+import javax.servlet.ServletException
 import javax.servlet.FilterChain
-import javax.servlet.ServletResponse
-import javax.servlet.ServletRequest
+import javax.servlet.http.HttpServletRequest
+import org.springframework.web.filter.OncePerRequestFilter
 
 
+@Component
+class CorsFilter : OncePerRequestFilter() {
 
-@Configuration
-class CorsFilter : Filter {
-    @Throws(IOException::class, ServletException::class)
-    override fun doFilter(req: ServletRequest, res: ServletResponse, chain: FilterChain) {
-        val response = res as HttpServletResponse
+    @Throws(ServletException::class, IOException::class)
+    override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain) {
         response.setHeader("Access-Control-Allow-Origin", "*")
-        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE")
-        response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type")
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
         response.setHeader("Access-Control-Max-Age", "3600")
-
-        chain.doFilter(req, res)
-
-    }
-
-    override fun destroy() {}
-
-    @Throws(ServletException::class)
-    override fun init(config: FilterConfig?) {
+        response.setHeader("Access-Control-Allow-Headers", "authorization, content-type, xsrf-token")
+        response.addHeader("Access-Control-Expose-Headers", "xsrf-token")
+        if ("OPTIONS" == request.method) {
+            response.status = HttpServletResponse.SC_OK
+        } else {
+            filterChain.doFilter(request, response)
+        }
     }
 }
