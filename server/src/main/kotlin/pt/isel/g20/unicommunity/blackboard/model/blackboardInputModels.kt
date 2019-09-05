e5930baff6d13c382ad7d25f24e3ff6f9bfccf0f
Sergiu Cozma <sergiucozma94@hotmail.com>
2019-09-06 00:35:24 +0100

    description is now optional for blackboard

--- server/src/main/kotlin/pt/isel/g20/unicommunity/blackboard/model/blackboardInputModels.kt
+++ server/src/main/kotlin/pt/isel/g20/unicommunity/blackboard/model/blackboardInputModels.kt
@@ -4,6 +4,6 @@ import com.fasterxml.jackson.annotation.JsonCreator
 
 data class BlackboardDto @JsonCreator constructor(
         val name: String,
-        val description: String,
-        val notificationLevel: String
+        val notificationLevel: String,
+        val description: String?
 )
